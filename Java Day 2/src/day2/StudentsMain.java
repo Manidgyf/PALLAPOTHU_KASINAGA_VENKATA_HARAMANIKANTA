c41f12f858a5fde76d0226bb5699b386cd3a1c8b
Manikanta <105136680+Manidgyf@users.noreply.github.com>
2024-08-30 16:12:22 +0530

    Add files via upload

---
+++ Java Day 2/src/day2/StudentsMain.java
@@ -0,0 +1,11 @@
+package day2;
+
+public class StudentsMain {
+	public static void main(String[] args) {
+		Students student=new Students(12,"manikanta");
+		System.out.println(student);
+		
+		
+	}
+
+}
